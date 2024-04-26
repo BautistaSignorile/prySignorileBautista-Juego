ddb2dbdcb51d88235f48c8987eed6075fc03fdbc
Bautista Signorile <bautistasignorile@gmail.com>
2024-04-25 22:52:04 -0300

    FaltaColisionYLaserEnemigo

--- frmMain.cs
+++ frmMain.cs
@@ -8,19 +8,13 @@ namespace prySignorileBautista_Juego
         claseNave objNaveEnemigo;
         claseNave objLaser;
         claseNave objLaserP;
+
+        List<claseNave> objList = new List<claseNave>();
+        List<claseNave> objEnemigo = new List<claseNave>();
         public frmMain()
         {
             InitializeComponent();
         }
-
-        private void frmMain_Load(object sender, EventArgs e)
-        {
-
-
-
-
-        }
-
         private void frmMain_Load_1(object sender, EventArgs e)
         {
             objNaveJugador = new claseNave();
@@ -29,15 +23,6 @@ namespace prySignorileBautista_Juego
 
             objNaveJugador.imgNave.Location = new Point(152, 380);
             Controls.Add(objNaveJugador.imgNave);
-
-            objLaserP = new claseNave();
-            objLaserP.crearLaserPlayer();
-
-            objLaserP.imgBalas.Location = new Point(objNaveJugador.imgNave.Location.X + 14, objNaveJugador.imgNave.Location.Y - objNaveJugador.imgNave.Size.Height);
-            Controls.Add(objLaserP.imgBalas);
-
-
-            //MessageBox.Show(objNaveJugador.nombre);
             objNaveEnemigo = new claseNave();
             int X = 17;
             for (int i = 0; i < 7; i++)
@@ -47,31 +32,52 @@ namespace prySignorileBautista_Juego
                 objNaveEnemigo.imgNaveEnemigo.Location = new Point(X, 20);
                 Controls.Add(objNaveEnemigo.imgNaveEnemigo);
                 X += objNaveEnemigo.imgNaveEnemigo.Size.Width + 17;
-
-                objLaser = new claseNave();
-                objLaser.crearLaserEnemigo();
-
-                objLaser.imgBala.Location = new Point(objNaveEnemigo.imgNaveEnemigo.Location.X + 12, objNaveEnemigo.imgNaveEnemigo.Location.Y + 20);
-                Controls.Add(objLaser.imgBala);
+                objEnemigo.Add(objNaveEnemigo);
             }
+            timer1.Start();
+            timer1.Enabled = true;
         }
-
         private void frmMain_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Right)
             {
                 objNaveJugador.imgNave.Location = new Point(objNaveJugador.imgNave.Location.X + 5, objNaveJugador.imgNave.Location.Y);
-                objLaserP.imgBalas.Location = new Point(objLaserP.imgBalas.Location.X + 5, objLaserP.imgBalas.Location.Y);
+                //objLaserP.imgBalas.Location = new Point(objLaserP.imgBalas.Location.X + 5, objLaserP.imgBalas.Location.Y);
             }
             if (e.KeyCode == Keys.Left)
             {
                 objNaveJugador.imgNave.Location = new Point(objNaveJugador.imgNave.Location.X - 5, objNaveJugador.imgNave.Location.Y);
-                objLaserP.imgBalas.Location = new Point(objLaserP.imgBalas.Location.X - 5, objLaserP.imgBalas.Location.Y);
+                //objLaserP.imgBala.Location = new Point(objLaserP.imgBala.Location.X - 5, objLaserP.imgBala.Location.Y);
+            }
+
+            if (e.KeyCode == Keys.Space)
+            {
+                objLaserP = new claseNave();
+                objLaserP.crearLaserPlayer();
+
+                objLaserP.imgBalas.Location = new Point(objNaveJugador.imgNave.Location.X + 15, objNaveJugador.imgNave.Location.Y - objNaveJugador.imgNave.Size.Height);
+                Controls.Add(objLaserP.imgBalas);
+                //objLaserP.imgBala.Location = new Point(objLaserP.imgBala.Location.X, objLaserP.imgBala.Location.Y);
+                objList.Add(objLaserP);
             }
         }
         private void timer1_Tick(object sender, EventArgs e)
         {
-            
+            foreach (claseNave bala in objList)
+            {
+                bala.imgBalas.Location = new Point(bala.imgBalas.Location.X, bala.imgBalas.Location.Y - 10);
+            }
+            foreach (claseNave bala in objList)
+            {
+                foreach (claseNave Enemigo in objEnemigo)
+                {
+                    if (bala.imgBalas.Bounds.IntersectsWith(Enemigo.imgNaveEnemigo.Bounds))
+                    {
+                        Enemigo.imgNaveEnemigo.Dispose();
+                        bala.imgBalas.Dispose();
+                    }
+                }
+            }
         }
     }
 }
